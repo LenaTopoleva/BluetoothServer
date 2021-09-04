df8acb166238d5b85ae18e8182fd1ff9b7109308
LenaKurasheva <lena.kurasheva@gmail.com>
2021-09-04 15:49:58 +0300

    now use ObjectOutputStream instead of InputStream with byte array

--- src/com/lenatopoleva/bluetoothserver/ProcessConnectionThread.java
+++ src/com/lenatopoleva/bluetoothserver/ProcessConnectionThread.java
@@ -4,7 +4,6 @@ import org.json.JSONObject;
 
 import javax.microedition.io.StreamConnection;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 
 public class ProcessConnectionThread implements Runnable {
 
@@ -20,75 +19,76 @@ public class ProcessConnectionThread implements Runnable {
     @Override
     public void run() {
         try {
-            byte[] message;
+            String message;
             // prepare to receive data
             OutputStream outputStream = mConnection.openOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
 
             System.out.println("Open output stream");
             Thread.sleep(3000);
 
             message = createMessageToSend("show_image", "other", "01.jpg");
-            outputStream.write(message);
+            objectOutputStream.writeObject(message);
             Thread.sleep(1000);
 
             message = createMessageToSend("tone_enable","","");
-            outputStream.write(message);
+            objectOutputStream.writeObject(message);
 
-            sendAllFilesFrom("./src/com/lenatopoleva/bluetoothserver/picturesaction", "show_image", "action", outputStream);
-            Thread.sleep(1000);
+            sendAllFilesFrom("./src/com/lenatopoleva/bluetoothserver/picturesaction", "show_image",
+                    "action", objectOutputStream);
 
             message = createMessageToSend("show_image", "other", "03.jpg");
-            outputStream.write(message);
-            Thread.sleep(1000);
-
-            message = createMessageToSend("show_image", "other", "02.jpg");
-            outputStream.write(message);
+            objectOutputStream.writeObject(message);
             Thread.sleep(1000);
 
             message = createMessageToSend("tone_disable","","");
-            outputStream.write(message);
+            objectOutputStream.writeObject(message);
 
-            sendAllFilesFrom("./src/com/lenatopoleva/bluetoothserver/picturesobject", "show_image", "object", outputStream);
-            Thread.sleep(1000);
+            message = createMessageToSend("show_image", "other", "02.jpg");
+            objectOutputStream.writeObject(message);
+
+            sendAllFilesFrom("./src/com/lenatopoleva/bluetoothserver/picturesobject", "show_image",
+                    "object", objectOutputStream);
 
             message = createMessageToSend("show_image", "other", "03.jpg");
-            outputStream.write(message);
+            objectOutputStream.writeObject(message);
             Thread.sleep(1000);
 
             message = createMessageToSend("stop_session", "", "");
-            outputStream.write(message);
+            objectOutputStream.writeObject(message);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static byte[] createMessageToSend (String messageType, String fileSubtype, String fileName) {
+    private static String createMessageToSend (String messageType, String fileSubtype, String fileName) {
 
         JSONObject jsonString = new JSONObject()
                 .put("type", messageType)
                 .put("subtype", fileSubtype)
                 .put("fileName", fileName);
 
-        return jsonString.toString().getBytes(StandardCharsets.UTF_8);
+        return jsonString.toString();
     }
 
 
-    private static void sendAllFilesFrom(String path, String messageType, String fileSubtype, OutputStream outputStream) throws IOException, InterruptedException {
+    private static void sendAllFilesFrom(String path, String messageType, String fileSubtype,
+                                         ObjectOutputStream objectOutputStream) throws IOException, InterruptedException {
         File[] fileArray = new File(path).listFiles();
         if (fileArray != null) {
             for (int i = 0; i < FILES_FOR_TEST_COUNT; i++) {
                 File file = fileArray[i];
                 if (messageType.equals("show_image") && file.getName().endsWith(".jpg")) {
                     System.out.println("file name = " + file.getName());
-                    byte[] message = createMessageToSend(messageType, fileSubtype, file.getName());
-                    outputStream.write(message);
+                    String message = createMessageToSend(messageType, fileSubtype, file.getName());
+                    objectOutputStream.writeObject(message);
                     Thread.sleep(1000);
 
                 }
                 if (messageType.equals("play_audio")) {
-                    byte[] message = createMessageToSend(messageType, fileSubtype, file.getName());
-                    outputStream.write(message);
+                    String message = createMessageToSend(messageType, fileSubtype, file.getName());
+                    objectOutputStream.writeObject(message);
                     Thread.sleep(1000);
                     System.out.println("Send " + file.getName() + " sound.");
                 }
